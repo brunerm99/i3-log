4bf2e0053c4f105f8f6fc38801ebc361ef3991a7
Marshall Bruner <brunerm99@gmail.com>
2023-10-10 17:20:44 +0200

    RunCommand added

--- src/ipc.rs
+++ src/ipc.rs
@@ -28,22 +28,30 @@ pub mod i3_ipc {
         output: String,
     }
 
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct CommandResponse {
+        success: bool,
+        #[serde(default)]
+        parse_error: bool,
+    }
+
     #[derive(Debug)]
     pub enum I3ResponseType {
+        RunCommand(Vec<CommandResponse>),
         GetWorkspace(Vec<WorkspaceResponse>),
         None,
     }
 
     #[derive(Debug)]
     pub enum I3MessageType {
-        Exit = 0,
+        RunCommand = 0,
         GetWorkspace = 1,
     }
 
     impl I3MessageType {
         fn from_u32(value: u32) -> I3MessageType {
             match value {
-                0 => I3MessageType::Exit,
+                0 => I3MessageType::RunCommand,
                 1 => I3MessageType::GetWorkspace,
                 _ => I3MessageType::GetWorkspace,
             }
@@ -51,28 +59,36 @@ pub mod i3_ipc {
     }
 
     pub trait I3Command {
-        fn send_i3_command(&mut self, message_type: u32, payload: &str) -> io::Result<Vec<u8>>;
+        fn send_i3_command(
+            &mut self,
+            message_type: I3MessageType,
+            payload: &str,
+        ) -> io::Result<Vec<u8>>;
         fn recv_i3_command(&mut self) -> io::Result<(I3MessageType, Vec<u8>)>;
     }
 
     impl I3Command for UnixStream {
-        fn send_i3_command(&mut self, message_type: u32, payload: &str) -> io::Result<Vec<u8>> {
-            // Message bytes:
-            //     00 - 05: "i3-ipc"
-            //     06 - 09: message length (little endian)
-            //     10 - 14: message type 0 - 12 (little endian)
-            //     15 - xx: payload (big endian?)
+        // Message bytes:
+        //     00 - 05: "i3-ipc"
+        //     06 - 09: message length (little endian)
+        //     10 - 14: message type 0 - 12 (little endian)
+        //     15 - xx: payload (big endian?)
+        fn send_i3_command(
+            &mut self,
+            message_type: I3MessageType,
+            payload: &str,
+        ) -> io::Result<Vec<u8>> {
             let mut message: Vec<u8> = Vec::with_capacity(6 + 4 + 4 + payload.len());
             message.extend_from_slice("i3-ipc".as_bytes()); // 00 - 05
             message.extend((payload.len() as u32).to_le_bytes()); // 06 - 09
-            message.extend(message_type.to_le_bytes()); // 10 - 14
+            message.extend((message_type as u32).to_le_bytes()); // 10 - 14
             message.extend_from_slice(payload.as_bytes()); // 15 - xx
             self.write_all(&message)?;
             Ok(message)
         }
 
+        // Check for 'i3-ipc', get message length and type, get message
         fn recv_i3_command(&mut self) -> io::Result<(I3MessageType, Vec<u8>)> {
-            // Check for 'i3-ipc', get message length and type, get message
             let mut i3_ipc_check = [0u8; 6];
             self.read_exact(&mut i3_ipc_check)?;
             if String::from_utf8_lossy(&i3_ipc_check) != String::from("i3-ipc") {
@@ -101,6 +117,10 @@ pub mod i3_ipc {
         response: &str,
     ) -> serde_json::Result<I3ResponseType> {
         match message_type {
+            I3MessageType::RunCommand => {
+                let response_json: Vec<CommandResponse> = serde_json::from_str(&response)?;
+                Ok(I3ResponseType::RunCommand(response_json))
+            }
             I3MessageType::GetWorkspace => {
                 let response_json: Vec<WorkspaceResponse> = serde_json::from_str(&response)?;
                 Ok(I3ResponseType::GetWorkspace(response_json))
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,21 @@ use std::io;
 
 fn main() -> io::Result<()> {
     let mut stream = i3_ipc::get_stream()?;
-    let message = stream.send_i3_command(i3_ipc::I3MessageType::GetWorkspace as u32, "exit")?;
+
+    // GetWorkspace testing
+    let message = stream.send_i3_command(i3_ipc::I3MessageType::GetWorkspace, "")?;
     println!("Sent message:\n{}", pretty_hex(&message));
     let (recv_message_type, recv_message) = stream.recv_i3_command()?;
     println!("Message received: {:#?}", recv_message_type);
-
     let response = String::from_utf8_lossy(&recv_message); // TODO: Move this to recv_i3_command()
-                                                           // let recv_json: serde_json::Value = serde_json::from_str(&recv_message)?;
-                                                           // println!("{:#?}", recv_json);
+    let response_json = i3_ipc::response_to_json(recv_message_type, &response)?;
+    println!("{:#?}", response_json);
 
+    // RunCommand testing
+    let message = stream.send_i3_command(i3_ipc::I3MessageType::RunCommand, "workspace 8")?;
+    println!("Sent message:\n{}", pretty_hex(&message));
+    let (recv_message_type, recv_message) = stream.recv_i3_command()?;
+    let response = String::from_utf8_lossy(&recv_message); // TODO: Move this to recv_i3_command()
     let response_json = i3_ipc::response_to_json(recv_message_type, &response)?;
     println!("{:#?}", response_json);
 
